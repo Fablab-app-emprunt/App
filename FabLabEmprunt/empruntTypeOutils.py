e7e75281812a7ddc46dd60f10ca67ee390f48d28
Heronal <antoine.lasserre@2024.icam.fr>
2022-04-15 16:07:46 +0200

    rajout retourOutil

--- FabLabEmprunt/empruntTypeOutils.py
+++ FabLabEmprunt/empruntTypeOutils.py
@@ -1,11 +1,14 @@
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QRadioButton, QPushButton
 from PyQt5.uic import loadUi
-
+from PyQt5 import QtGui,QtCore
 
 class EmpruntTypeOutils(QWidget):
     def __init__(self):
         super(EmpruntTypeOutils, self).__init__()
         loadUi('empruntTypeOutils.ui', self)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap('Capture d’écran 2022-04-14 153417.png'))
+        self.boutonaccueil.setIcon(icon)
 
 
 
