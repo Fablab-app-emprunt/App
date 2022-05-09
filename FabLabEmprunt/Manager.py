4ef215f8686d1c7f9b7a76fb5659d91ecb9108ed
Dorian-LB <dorian.le-boulch@2024.icam.fr>
2022-05-09 08:39:29 +0200

    BDD avancement

--- FabLabEmprunt/Manager.py
+++ FabLabEmprunt/Manager.py
@@ -7,8 +7,7 @@ from PyQt5.QtSql import QSql, QSqlQuery, QSqlDatabase
 import mysql.connector
 
 global requete_Outils
-requete_Outils = 'ELEC'
-
+requete_Outils = ''
 class PageAccueil(QWidget):
     def __init__(self):
         super(PageAccueil,self).__init__()
@@ -35,37 +34,16 @@ class EmpruntTypeOutils(QWidget):
         self.bouton_bois.clicked.connect(self.bois)
         self.bouton_usinage.clicked.connect(self.usinage)
         self.boutonaccueil.clicked.connect(self.accueil)
+        # self.requete_Outils = requete_Outils
 
 
     def accueil(self):
         widget.setCurrentIndex(0)
 
-    def conn_BDD(self):
-    def bois(self):
-        widget.setCurrentIndex(2)
-        requete_Outils = 'BOIS'
-
-    def elec(self):
-        widget.setCurrentIndex(2)
-        requete_Outils = 'ELEC'
-    def usinage(self):
-        widget.setCurrentIndex(2)
-        requete_Outils = 'USINAGE'
-
-class EmpruntAjout(QWidget, QSqlDatabase):
-    def __init__(self):
-        super(EmpruntAjout, self).__init__()
-        loadUi('empruntAjout.ui',self)
-        self.boutonaccueil.clicked.connect(self.accueil)
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap('Capture d’écran 2022-04-14 153417.png'))
-        self.boutonaccueil.setIcon(icon)
-        self.today.clicked.connect(self.date)
-        self.dateemprunt.clicked.connect(self.date)
-
+    def conn_BDD(self, requete_Outils):
         #---------BDD----------------
 
-    #-----------Déclaration de la connexion---------
+        #-----------Déclaration de la connexion---------
         connection = mysql.connector.connect(user='u556968436_LaTeamDeLoick', password='LoickRaison2022',
                                              host='145.14.151.101',
                                              database='u556968436_fablab')
@@ -81,28 +59,60 @@ class EmpruntAjout(QWidget, QSqlDatabase):
         #     print("Id = ", row[1], )
         #
         # connection.close()
-        count_tools = "select count(*) from Outils where departOutils_Outils = ' " + requete_Outils + "'"
+        count_tools = "select count(*) from Outils where departOutils_Outils = '" + requete_Outils + "'"
         cursor = connection.cursor()
         cursor.execute(count_tools)
         outils = cursor.fetchall()
         for row in outils:
             print("Id = ", row[0], )
         connection.close()
-    #----------Requêtes de modification------------
+        #----------Requêtes de modification------------
 
         # query = QSqlQuery("insert into Utilisateur (login) values ('david')")
 
         #                OU
 
-    #         # query = QSqlQuery()
-    #         # query.exec_("insert into Utilisateur (login) values ('david')")
-    #
-    #     #----------Requêtes préparées------------ (utile lorsqu'on utilise des variables)
-    #
-    #         # query = QSqlQuery()
-    #         # query.prepare("insert into Utilisateur (login) values (:nom)")
+        #         # query = QSqlQuery()
+        #         # query.exec_("insert into Utilisateur (login) values ('david')")
+        #
+        #     #----------Requêtes préparées------------ (utile lorsqu'on utilise des variables)
+        #
+        #         # query = QSqlQuery()
+        #         # query.prepare("insert into Utilisateur (login) values (:nom)")
         # query.bindValue(":nom", "david")
         # query.exec_()
+
+        #-----------Gestion des erreurs---------------
+
+        # db.open("login", "motdepasse")
+        # if db.isOpenError():
+        #     error = db.lastError()
+
+
+    def bois(self):
+        widget.setCurrentIndex(2)
+        requete_Outils = 'BOIS'
+
+    def elec(self):
+        widget.setCurrentIndex(2)
+        requete_Outils = 'ELEC'
+        self.conn_BDD(requete_Outils)
+
+    def usinage(self):
+        widget.setCurrentIndex(2)
+        requete_Outils = 'USINAGE'
+
+class EmpruntAjout(QWidget, QSqlDatabase):
+    def __init__(self):
+        super(EmpruntAjout, self).__init__()
+        loadUi('empruntAjout.ui',self)
+        self.boutonaccueil.clicked.connect(self.accueil)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap('Capture d’écran 2022-04-14 153417.png'))
+        self.boutonaccueil.setIcon(icon)
+        self.today.clicked.connect(self.date)
+        self.dateemprunt.clicked.connect(self.date)
+
 
     def accueil(self):
        widget.setCurrentIndex(0)
@@ -110,12 +120,6 @@ class EmpruntAjout(QWidget, QSqlDatabase):
     def date(self):
         value = self.today.selectedDate()
         return value
-
-    #-----------Gestion des erreurs---------------
-
-        # db.open("login", "motdepasse")
-        # if db.isOpenError():
-        #     error = db.lastError()
 
 if __name__ == "__main__":
 
