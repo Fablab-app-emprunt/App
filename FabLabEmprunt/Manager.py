5cfd2220dfd64a324b58987db2c76cb212211209
Dorian-LB <dorian.le-boulch@2024.icam.fr>
2022-05-09 14:23:58 +0200

    Tableau fonctionnel

--- FabLabEmprunt/Manager.py
+++ FabLabEmprunt/Manager.py
@@ -10,8 +10,10 @@ from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QWidget, QMessageBox,
 from PyQt5.QtSql import QSql, QSqlQuery, QSqlDatabase
 import mysql.connector
 
-global requete_Outils
-global Data
+
+state = ''
+requete_Outils = ''
+Data = []
 
 class PageAccueil(QWidget):
     def __init__(self):
@@ -23,9 +25,15 @@ class PageAccueil(QWidget):
 
     def goToRendreOutils(self):
         widget.setCurrentIndex(1)
+        global state
+        state = 'Rendre'
+        print(state)
 
     def goToEmprunterOutils(self):
         widget.setCurrentIndex(1)
+        global state
+        state = 'Emprunter'
+        print(state)
 
 
 class EmpruntTypeOutils(QWidget):
@@ -43,66 +51,33 @@ class EmpruntTypeOutils(QWidget):
 
     def accueil(self):
         widget.setCurrentIndex(0)
-
-    def conn_BDD(self, requete_Outils):
-        #---------BDD----------------
-
-        #-----------Déclaration de la connexion---------
-        connection = mysql.connector.connect(user='u556968436_LaTeamDeLoick', password='LoickRaison2022',
-                                             host='145.14.151.101',
-                                             database='u556968436_fablab')
-        # sql_select_Query = "select * from Personnes"
-        # cursor = connection.cursor()
-        # cursor.execute(sql_select_Query)
-        # # get all records
-        # records = cursor.fetchall()
-        # print("Total number of rows in table: ", cursor.rowcount)
-        #
-        # print("\nPrinting each row")
-        # for row in records:
-        #     print("Id = ", row[1], )
-        #
-        # connection.close()
-        #----------Requêtes de modification------------
-
-        # query = QSqlQuery("insert into Utilisateur (login) values ('david')")
-
-        #                OU
-
-        #         # query = QSqlQuery()
-        #         # query.exec_("insert into Utilisateur (login) values ('david')")
-        #
-        #     #----------Requêtes préparées------------ (utile lorsqu'on utilise des variables)
-        #
-        #         # query = QSqlQuery()
-        #         # query.prepare("insert into Utilisateur (login) values (:nom)")
-        # query.bindValue(":nom", "david")
-        # query.exec_()
-
-        #-----------Gestion des erreurs---------------
-
-        # db.open("login", "motdepasse")
-        # if db.isOpenError():
-        #     error = db.lastError()
-
+        print("Index Accueil : ",widget.currentIndex())
 
     def bois(self):
-        widget.setCurrentIndex(2)
+        global requete_Outils
         requete_Outils = 'BOIS'
-        self.conn_BDD(requete_Outils)
+        page_choix_outils = EmpruntAjout()
+        widget.addWidget(page_choix_outils)
+        widget.setCurrentIndex(2)
 
     def elec(self):
-        widget.setCurrentIndex(2)
+        global requete_Outils
         requete_Outils = 'ELEC'
-        self.conn_BDD(requete_Outils)
+        page_choix_outils = EmpruntAjout()
+        widget.addWidget(page_choix_outils)
+        widget.setCurrentIndex(2)
 
     def usinage(self):
+        global requete_Outils
         widget.setCurrentIndex(2)
         requete_Outils = 'USINAGE'
-        self.conn_BDD(requete_Outils)
+        page_choix_outils = EmpruntAjout()
+        widget.addWidget(page_choix_outils)
+        widget.setCurrentIndex(2)
 
 class EmpruntAjout(QWidget, QSqlDatabase):
     def __init__(self):
+
         super(EmpruntAjout, self).__init__()
         loadUi('empruntAjout.ui',self)
         self.boutonaccueil.clicked.connect(self.accueil)
@@ -112,20 +87,25 @@ class EmpruntAjout(QWidget, QSqlDatabase):
         self.today.clicked.connect(self.date)
         self.dateemprunt.clicked.connect(self.date)
 
-        self.table_Emprunt.setColumnCount(2)
+        global state
+        global requete_Outils
+        global Data
+        print(requete_Outils)
+        print("valeur : ",state)
+        self.table_Emprunt.setColumnCount(4)
         self.table_Emprunt.rowCount()
-        self.table_Emprunt.setHorizontalHeaderLabels(['id','Name','reference'])
+        self.table_Emprunt.setHorizontalHeaderLabels(['id','Name','Quantity',state])
         row = self.table_Emprunt.rowCount()
         self.table_Emprunt.setRowCount(row+1)
         col = 0
         connection = mysql.connector.connect(user='u556968436_LaTeamDeLoick', password='LoickRaison2022',
                                              host='145.14.151.101',
                                              database='u556968436_fablab')
-        count_tools = "select * from Outils where departOutils_Outils = 'ELEC'"
+
+        count_tools = "select * from Outils where departOutils_Outils = '"+requete_Outils+"'"
         cursor = connection.cursor()
         cursor.execute(count_tools)
         Data = cursor.fetchall()
-        connection.close()
         for item in Data:
             cell = QTableWidgetItem(str(item[0]))
             self.table_Emprunt.setItem(row, col, cell)
@@ -135,11 +115,6 @@ class EmpruntAjout(QWidget, QSqlDatabase):
 
             row += 1
             self.table_Emprunt.setRowCount(row+1)
-
-
-
-
-
 
     def accueil(self):
        widget.setCurrentIndex(0)
@@ -154,17 +129,13 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = QtWidgets.QStackedWidget()
 
-    # from FabLabEmprunt.pageAccueil import *
-    # from FabLabEmprunt.empruntTypeOutils import *
-    # from FabLabEmprunt.empruntajout import *
-
     page_accueil = PageAccueil()
     page_choix_type_outils = EmpruntTypeOutils()
-    page_choix_outils = EmpruntAjout()
+    # page_choix_outils = EmpruntAjout()
 
     widget.addWidget(page_accueil)
     widget.addWidget(page_choix_type_outils)
-    widget.addWidget(page_choix_outils)
+    # widget.addWidget(page_choix_outils)
 
     widget.setFixedHeight(700)
     widget.setFixedWidth(1000)
--- FabLabEmprunt/main.py
+++ FabLabEmprunt/main.py
@@ -7,15 +7,34 @@ from PyQt5.QtSql import QSql, QSqlQuery, QSqlDatabase
 import mysql.connector
 
 global requete_Outils
+global widget
+
+# class MainApp(QWidget):
+#     def __init__(self):
+#         super(MainApp, self).__init__()
+#         self.Stack = QtWidgets.QStackedWidget()
+#         widget = QtWidgets.QStackedWidget()
+#
+#         self.page_accueil = PageAccueil()
+#         self.page_choix_type_outils = EmpruntTypeOutils()
+#         self.page_choix_outils = EmpruntAjout()
+#
+#         self.widget.addWidget(self.page_accueil)
+#         self.widget.addWidget(self.page_choix_type_outils)
+#         self.widget.addWidget(self.page_choix_outils)
+#
+#         widget.setFixedHeight(700)
+#         widget.setFixedWidth(1000)
+#         widget.show()
+# if __name__ == "__main__":
+
+app = QApplication(sys.argv)
+# IHM = MainApp()
 
 from FabLabEmprunt.pageAccueil import PageAccueil
 from FabLabEmprunt.empruntTypeOutils import EmpruntTypeOutils
 from FabLabEmprunt.empruntajout import EmpruntAjout
 
-
-# if __name__ == "__main__":
-
-app = QApplication(sys.argv)
 widget = QtWidgets.QStackedWidget()
 
 page_accueil = PageAccueil()
