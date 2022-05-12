1b994c4b505510671920197db1037c657001dc5b
loftinck <loick.defretin@2024.icam.fr>
2022-05-12 15:38:23 +0200

    début RendreOutils fonctionnel

--- FabLabEmprunt/Manager.py
+++ FabLabEmprunt/Manager.py
@@ -273,7 +273,51 @@ class RendreOutils(QWidget, QSqlDatabase):
         icon.addPixmap(QtGui.QPixmap('Capture d’écran 2022-04-14 153417.png'))
         self.boutonaccueil.setIcon(icon)
         self.return_bouton.clicked.connect(self.accueil)
+        self.table_Emprunt.setColumnCount(4)
+        self.table_Emprunt.rowCount()
+        self.table_Emprunt.setHorizontalHeaderLabels(['id',"Nom de l'outils",'Quantité','Rendre'])
 
+        row = self.table_Emprunt.rowCount()
+        self.table_Emprunt.setRowCount(row+1)
+        col = 0
+        connection = mysql.connector.connect(user='u556968436_LaTeamDeLoick', password='LoickRaison2022',
+                                             host='145.14.151.101',
+                                             database='u556968436_fablab')
+
+        count_tools = "select * from Outils where quantiteEmprunte_Outils != 0"
+        cursor = connection.cursor()
+        cursor.execute(count_tools)
+        Data = cursor.fetchall()
+
+        #-------------REMPLIR LES TABLEAUX---------------------
+        for item in Data:
+            cell = QTableWidgetItem(str(item[0]))
+            self.table_Emprunt.setItem(row, col, cell)
+
+            cell = QTableWidgetItem(str(item[1]))
+            self.table_Emprunt.setItem(row, col+1, cell)
+            #-----------3EME COLONNE : FAIRE UNE LISTE DEROULANTE POUR CHOISIR LA QUANTITE DE CHAQUUE OUTIL A RENDRE---------------
+            L = []
+            for i in range(item[5] + 1):
+                L.append(str(i))
+                i+=1
+            combo = QComboBox()
+            for i in L:
+                combo.addItem(i)
+            self.table_Emprunt.setCellWidget(row, col+2, combo)
+
+            chkBoxItem = QTableWidgetItem()
+            chkBoxItem.setFlags(QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsEnabled)
+            chkBoxItem.setCheckState(QtCore.Qt.Unchecked)
+            self.table_Emprunt.setItem(row,col+3,chkBoxItem)
+            row += 1
+            self.table_Emprunt.setRowCount(row+1)
+
+            header = self.table_Emprunt.horizontalHeader()
+            header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+            header.setSectionResizeMode(0, QtWidgets.QHeaderView.ResizeToContents)
+            header.setSectionResizeMode(2, QtWidgets.QHeaderView.ResizeToContents)
+            header.setSectionResizeMode(3, QtWidgets.QHeaderView.ResizeToContents)
 
     def accueil(self):
         widget.setCurrentIndex(0)
